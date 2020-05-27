498a2d72035f6ab89efc0cc06ec86b4db7cdb8bc
= <=>
2020-05-27 17:11:35 -0300

    ajustes

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,14 +1,16 @@
-$(function() {
+$(function () {
 
-    $("#subscribe input,#subscribe email").jqBootstrapValidation({
+    $("#subscribe input,#subscribe emailSub").jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function($form, event, errors) {
+        submitError: function ($form, event, errors) {
             // additional error messages or events
         },
-        submitSuccess: function($form, event) {
+        submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var email = $("input#email").val();
+            var email = $("input#emailSub").val();
+            
+            console.log(email);
 
             $.ajax({
                 url: "https://www.cyberoficina.com.br/subscribe",
@@ -18,37 +20,39 @@ $(function() {
                     email: email
                 },
                 cache: false,
-                success: function() {
+                success: function () {
                     // Success message
-                    $('#successSubscribe').html("<div class='alert alert-success'>");
+                    $('#successSubscribe').html("<div style='position: absolute; width: 90%' class='mt-2 alert alert-success'>");
                     $('#successSubscribe > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
+                            .append("</button>");
                     $('#successSubscribe > .alert-success')
-                        .append("<strong>Em breve você receberá nossas novidades! </strong>");
+                            .append("<strong>Obrigado! Em breve você receberá nossas novidades. </strong>");
                     $('#successSubscribe > .alert-success')
-                        .append('</div>');
+                            .append('</div>');
+                    $('#successSubscribe').delay(5000).fadeOut('slow');
+
 
                     //clear all fields
-                    $('#subscribe').trigger("reset");
+                    $('#successSubscribe').trigger("reset");
                 },
-                error: function() {
+                error: function () {
                     // Fail message
                     $('#successSubscribe').html("<div class='alert alert-danger'>");
                     $('#successSubscribe > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#successSubscribe > .alert-danger').append("<strong>Desculpe, aparentemente nossos servidores não estão respondendo. Por favor tente novamente mais tarde!");
+                            .append("</button>");
+                    $('#successSubscribe > .alert-danger').append("<strong>Desculpe " + email + ", aparentemente nossos servidores não estão respondendo. Por favor tente novamente mais tarde!");
                     $('#successSubscribe > .alert-danger').append('</div>');
                     //clear all fields
                     $('#successSubscribe').trigger("reset");
                 },
             })
         },
-        filter: function() {
+        filter: function () {
             return $(this).is(":visible");
         },
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+    $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
     });
@@ -56,6 +60,6 @@ $(function() {
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#email').focus(function() {
+$('#emailSub').focus(function () {
     $('#successSubscribe').html('');
 });
